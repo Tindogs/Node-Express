5f6912c2f1e996a8c0fe878f1be67472ace7f65d
Fco. Javier Guerrero Carmona <fjguerreroc@icloud.com>
2018-03-14 12:56:55 +0100

    Put Method for Dog - Get Dog by UserID - Update User

--- routes/apiv1/users.js
+++ routes/apiv1/users.js
@@ -20,6 +20,32 @@ router.get('/', (req, res, next) => {
     });
 });
 
+// Find User By UserId
+router.get('/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, users) => {
+        if (err) {
+            next(err);
+            return
+        }
+
+        res.json({ success: true, result: users });
+
+    });
+});
+
+// Find Dogs By UserId
+router.get('/dogs/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, users) => {
+        if (err) {
+            next(err);
+            return
+        }
+
+        res.json({ success: true, result: users.dogs });
+
+    });
+});
+
 /* POST /apiv1/users */
 router.post('/', (req, res, next) => {
     //console.log(req.body);
@@ -32,8 +58,8 @@ router.post('/', (req, res, next) => {
             return;
         }
         res.json({ success: true, result: userSave, token: tokens.createToken(user) }); 
-    })
-})
+    });
+});
 
 router.post('/authenticate', (req, res, next)=>{
     const user = req.body;
@@ -53,6 +79,54 @@ router.post('/authenticate', (req, res, next)=>{
                 res.json({success: true, token: tokens.createToken(data), username: data.username, dogs: data.dogs});
             });
         }
+});
+
+/* New Dog or Update Dog By UserId */
+router.put('/dogs/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, userUpdate) => {
+
+        userUpdate.dogs = req.body.dogs
+        //console.log(userUpdate.dogs)
+        
+        for (var item in req.body.dogs){
+            userUpdate.dogs[item].name = req.body.dogs[item].name
+            userUpdate.dogs[item].age = req.body.dogs[item].age
+            userUpdate.dogs[item].breed = req.body.dogs[item].breed
+            userUpdate.dogs[item].purebreed = req.body.dogs[item].purebreed
+            userUpdate.dogs[item].color = req.body.dogs[item].color
+            userUpdate.dogs[item].description = req.body.dogs[item].description
+            userUpdate.dogs[item].photos = req.body.dogs[item].photos
+        }
+        
+        userUpdate.save((err, userSave) => {
+            if(err){
+                next(err);
+                return;
+            }
+            res.json({success: true, result: userSave})
+        });
+    });
+});
+
+
+/* Update User */
+router.put('/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, userUpdate) => {
+        userUpdate.first_name = req.body.first_name;
+        userUpdate.last_name = req.body.last_name;
+        userUpdate.email = req.body.email
+        userUpdate.username = req.body.username
+        userUpdate.password = req.body.password
+        userUpdate.coordinates = req.body.coordinates
+        
+        userUpdate.save((err, userSave) => {
+            if(err){
+                next(err);
+                return;
+            }
+            res.json({success: true, result: userSave})
+        });
+    });
 });
 
 module.exports = router;
