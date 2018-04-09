548bb66762e45504b9e3e96a48625d5cc74ccca5
Carolina Calvo <pececillu@gmail.com>
2018-04-09 13:43:48 +0200

    Documentación búsqueda perros

--- routes/apiv1/users/dogs/index.js
+++ routes/apiv1/users/dogs/index.js
@@ -39,7 +39,6 @@ router.put('/', auth, require('./dog-register'));
 //     "likes_from_others": [],
 //     "photos": [],
 //     "name": "Dog's name",
-//     "age": XX,
 //     "breed": "Dog´s breed",
 //     "purebreed": false,
 //     "color": "Dog´s color",
@@ -50,5 +49,11 @@ router.put('/', auth, require('./dog-register'));
 //     "querybreed": "Dog`s breed"
 // }
 router.put('/:dogId', auth, require('./dog-update'));
+
+/* Find Dogs By UserId */
+// GET HTTP METHOD
+// Needs a valid token, a pair of key:value named token at the header
+// EXAMPLE: http://localhost:3000/apiv1/users/5aa997a2d5d9b8046a908253/dogs/5ab7c85875b8dcb2053db1e0/search
 router.get('/:dogId/search', auth, require('./dog-search'));
+
 module.exports = router;
